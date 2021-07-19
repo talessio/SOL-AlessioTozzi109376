14857b2b5a391948e1218b3dd986f96527217998
talessio <81319268+talessio@users.noreply.github.com>
2021-07-19 12:39:45 +0200

    cmake build from terminal

---
+++ build/CMakeFiles/bvernan.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bvernan.dir/bvernan.c.o"
+  "CMakeFiles/bvernan.dir/bvernan.c.o.d"
+  "CMakeFiles/bvernan.dir/main.c.o"
+  "CMakeFiles/bvernan.dir/main.c.o.d"
+  "bvernan"
+  "bvernan.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bvernan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
