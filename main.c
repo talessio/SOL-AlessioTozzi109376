b565f3e2585d33725e81eb9c2a6ef6a2df8962f4
talessio <81319268+talessio@users.noreply.github.com>
2021-07-20 12:43:22 +0200

    fixed main

--- main.c
+++ main.c
@@ -11,21 +11,16 @@ int main(int argc, char **argv) {
     clock_t begin = clock();
     printf("Working on it...\n");
 //    encryptDecrypt("k.txt", "input.txt", "output.txt");
-    if (argc != 5) {
-        printf("Wrong number of parameters, %d instead of 5!\n\n", argc);
+    if (argc != 4) {
+        printf("Wrong number of parameters, %d instead of 4!\n\n", argc);
         printInfo();
         return -1;
     }
-    if (strcmp(argv[1], "bvernan")!=0) {
-        printf("Wrong command!\n\n");
-        printInfo();
-        return -1;
-    }
-    if (argv[3] == NULL || argv[4] == NULL){
+    if (argv[1] == NULL || argv[2] == NULL){
         perror("Error! Couldn't open file\n");
         return -1;
     }
-    encryptDecrypt(argv[2], argv[3], argv[4]);
+    encryptDecrypt(argv[1], argv[2], argv[3]);
     clock_t end = clock();
     double tElapsed = (double)(end - begin) / CLOCKS_PER_SEC;
     printf("It took me %lf seconds.\n", tElapsed);
